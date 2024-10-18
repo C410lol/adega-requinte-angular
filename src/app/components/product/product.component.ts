05f72c00a420f05ae7993abc482c2dcced9b9eb9
C410lol <gcaio7463@gmail.com>
2024-10-18 16:44:02 -0300

    Incrementing section for member price discount

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { WineType } from '../../types/WineType';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
+import { formatPriceNumber } from '../../constants/Utils';
 
 @Component({
   selector: 'app-product',
@@ -27,6 +28,14 @@ export class ProductComponent {
   getFirstImage(): string {
     if (this.wine.images == null || this.wine.images.length < 1) return '../../assets/wine_img.png';
     return this.wine.images[0];
+  }
+
+  getMemberPrice(): number {
+    return parseFloat(((this.wine.regPrice * 90) / 100).toFixed(2));
+  }
+
+  getNumberFormated(number?: number): string {
+    return formatPriceNumber(number);
   }
 
 
